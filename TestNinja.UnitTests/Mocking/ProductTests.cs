afbc4dd94e22dc5bff4ecc5899c2acc9fc06325d
HenriqueSD <hsd.foz@gmail.com>
2019-03-31 18:57:24 -0300

    Created ProductTests class

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        // This is the ideal;
+        [Test]
+        public void GetPrice_Gold_Customer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+        // Example of abusing mock;
+        //[Test]
+        //public void GetPrice_Gold_Customer_Apply30PercentDiscount2()
+        //{
+        //    var customer = new Mock<ICustomer>();
+        //    customer.Setup(c => c.IsGold).Returns(true);
+
+        //    var product = new Product { ListPrice = 100 };
+        //    var result = product.GetPrice(customer.Object);
+
+        //    Assert.That(result, Is.EqualTo(70));
+        //}
+    }
+}
--- TestNinja/Mocking/Product.cs
+++ TestNinja/Mocking/Product.cs
@@ -11,10 +11,25 @@
 
             return ListPrice;
         }
+
+        // This commented code was used for example of abusing mock;
+        //public float GetPrice(ICustomer customer)
+        //{
+        //    if (customer.IsGold)
+        //        return ListPrice * 0.7f;
+
+        //    return ListPrice;
+        //}
     }
 
     public class Customer
     {
         public bool IsGold { get; set; }
     }
+
+    // This commented code was used for example of abusing mock;
+    //public interface ICustomer
+    //{
+    //    bool IsGold { get; set; }
+    //}
 }
