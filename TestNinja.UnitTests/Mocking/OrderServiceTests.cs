fd043edb5c50a7149656a12fc0d018a6f151b51e
HenriqueSD <hsd.foz@gmail.com>
2019-03-31 18:47:09 -0300

    Created OrderServiceTests class

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+
+            var service = new OrderService(storage.Object);
+            var order = new Order();
+
+            service.PlaceOrder(order);
+
+            // To test interaction between two objects, you can use the verify method of mock objects;
+            // With verify method you can verify if a given method is called with the right arguments or not;
+            storage.Verify(s => s.Store(order));
+
+        }
+    }
+}
