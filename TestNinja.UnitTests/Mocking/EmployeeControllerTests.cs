09129fa2fc6ad94bd7829a6806c1d2f8fa6d19d6
HenriqueSD <hsd.foz@gmail.com>
2019-04-01 19:43:29 -0300

    Created EmployeeStorage class and EmployeeControllerTests class

---
+++ TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            // in this case we don't care about the result, we just wanna test the
+            // interaction of this controller with the storage object;
+            controller.DeleteEmployee(1);
+
+            storage.Verify(s => s.DeleteEmployy(1));
+        }
+    }
+}
---
+++ TestNinja/Mocking/EmployeeStorage.cs
@@ -0,0 +1,26 @@
+﻿namespace TestNinja.Mocking
+{
+    public interface IEmployeeStorage
+    {
+        void DeleteEmployy(int id);
+    }
+
+    public class EmployeeStorage : IEmployeeStorage
+    {
+        private EmployeeContext _db;
+
+        public EmployeeStorage()
+        {
+            _db = new EmployeeContext();
+        }
+
+        public void DeleteEmployy(int id)
+        {
+            var employee = _db.Employees.Find(id);
+            if (employee == null) return;
+            _db.Employees.Remove(employee);
+            _db.SaveChanges();
+
+        }
+    }
+}
